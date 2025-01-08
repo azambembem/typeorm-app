de0bf564826eb76f823bdeace2b246d5f45dbd37
azambembem <azamjan980713@gmail.com>
2025-01-08 23:52:35 +0900

    TypeORM을 사용해서 앱과 DB 연결해보기 + Docker

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+// import { AppDataSource } from "./data-source"
+// import { User } from "./entity/User"
+
+// AppDataSource.initialize().then(async () => {
+
+//     console.log("Inserting a new user into the database...")
+//     const user = new User()
+//     user.firstName = "Timber"
+//     user.lastName = "Saw"
+//     user.age = 25
+//     await AppDataSource.manager.save(user)
+//     console.log("Saved a new user with id: " + user.id)
+
+//     console.log("Loading users from the database...")
+//     const users = await AppDataSource.manager.find(User)
+//     console.log("Loaded users: ", users)
+
+//     console.log("Here you can setup and run express / fastify / any other framework.")
+
+// }).catch(error => console.log(error))
+
+
+import express from "express";
+import morgan from "morgan";
+import { AppDataSource } from "./data-source";
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res)=> {
+    res.send("Hello, World!");
+})
+
+AppDataSource.initialize().then(()=> {
+    console.log("성공")
+})
+.catch((error) => console.log(error));
+
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
